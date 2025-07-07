bd844b0a290f3e839855a7294bba26b7722ebb66
송민석 <songminseok@helixtech.co.kr>
2025-07-07 14:26:19 +0900

    chore: mode에 따른 base 설정

--- slidev.config.js
+++ slidev.config.js
@@ -1,7 +1,7 @@
-export default {
+export default ({ mode, command }) => ({
   vite: {
-    base: process.env.NODE_ENV === 'production'
+      base: mode === 'production'
       ? '/learn-12-factors-app/'
       : '/',
   },
-}
+})
